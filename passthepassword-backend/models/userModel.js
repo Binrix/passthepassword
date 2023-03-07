fdf4f1f72a3dae11d7e6b0e8fed08ab2574561ea
Jan Galliker <jan.galliker@roche.com>
2023-03-07 10:25:14 +0100

    add forgotten changes to user schema for MongoDb

--- passthepassword-backend/models/userModel.js
+++ passthepassword-backend/models/userModel.js
@@ -4,11 +4,16 @@ let userSchema = new mongoose.Schema({
     username: String,
     password: String,
     createdAt: Date,
-    completedMemories: [String],
+    websites: [websiteSchema],
 });
 
 let websiteSchema = new mongoose.Schema({
-    
+    denomination: String,
+    url: String,
+    username: String,
+    usernameSalt: String,
+    password: String,
+    passwordSalt: String,
 })
 
 module.exports = new mongoose.model('User',userSchema);
